102598ad4579ae2a512405f3c25d1217886efb02
Walton <walton.ow@pg.com>
2019-11-21 16:44:27 +0000

    started part 1 excercises

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// create new app   $ npx create-react-app <app name>
+//                  $ cd part1
+// run application  $ npm start
 
-const Hello = (props) => {
+
+//WARNING create-react-app automatically makes the project a git repository unless the application is created within an already existing 
+// repository. Most likely you do not want the project becoming a repository, so run the command rm -rf .git in the root of the project.
+
+// React apps are created in JSX which resembles HTML, JSX is compiled into JS by Babel
+// JSX is XML-like so all tags need to be close e.g. <br />
+
+
+// react-component defined
+const Hello = (props) => { // data can be passed to components using props
     return (
         <div>
-            <p>GobbledyGookedy {props.name} = {props.age}</p>
+            <p>Gobbledy Gookedy {props.name} = {props.age}</p>
         </div>
     ) 
 }
 
-// is this a comment? 
-
+// this is a footer component
 const Footer = () => {
     return (
       <div>
@@ -22,6 +33,7 @@ const Footer = () => {
   }
 
 
+  // this is the main page application with various components contained inside
 const App = () => {
 console.log('hello bro')
 
@@ -31,14 +43,16 @@ const b = 20
 const name = 'Owen'
 const age = 27
 
+// outtermost div element required in order to properly render
 return (
-  <div>
+  <div> 
       <p>The data is: {now.toString()}</p>
       <br />
     <p>Hello world: {a} + {b} = {a+b}</p>
     <br />
     <Hello name={name} age={age} />
     <br />
+    <Hello name={'Geoff'} age={38} />
     < Footer />
   </div>
     )
@@ -49,4 +63,4 @@ return (
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root')) // this line renders the Reac-component App into the DOM in a div with id='root'
