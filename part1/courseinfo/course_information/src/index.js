4c83da979f34351ebfc2bd594a9a10ac144fdd19
Walton <walton.ow@pg.com>
2019-11-22 11:04:04 +0000

    part 1.5

--- part1/courseinfo/course_information/src/index.js
+++ part1/courseinfo/course_information/src/index.js
@@ -6,7 +6,7 @@ const Header = (props) => {
 
     return (
         <div>
-            <h1>{props.course}</h1>
+            <h1>{props.course.name}</h1>
         </div>
     )
 }
@@ -16,7 +16,7 @@ const Part = (props) => {
     return (
         <div>
             <p>
-                {props.part} {props.exercise}
+                {props.part.name} {props.exercises.exercises}
             </p>
         </div>
     )
@@ -26,9 +26,11 @@ const Content = (props) => {
 
     return (
         <div>
-            <Part part={props.part1} exercise={props.exercises1}/>
-            <Part part={props.part2} exercise={props.exercises2}/>
-            <Part part={props.part3} exercise={props.exercises3}/>
+            
+            <Part part={props.course.parts[0]} exercises={props.course.parts[0]} />
+            <Part part={props.course.parts[1]} exercises={props.course.parts[1]} />
+            <Part part={props.course.parts[2]} exercises={props.course.parts[2]} />
+            
         </div>
             )
 }
@@ -38,7 +40,7 @@ const Total = (props) => {
     return (
         <div>
             <p>
-                Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+                Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
             </p>
         </div>
     )
@@ -47,27 +49,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
+  const course = {
+  name: 'Half Stack application development',
+  parts: [
+    {
       name: 'Fundamentals of React',
       exercises: 10
-    }
-  const part2 = {
-      name: 'Using props to pass data', 
+    },
+    {
+      name: 'Using props to pass data',
       exercises: 7
-    }
-  const part3 = {
+    },
+    {
       name: 'State of a component',
       exercises: 14
     }
-
-    const part_array = [part1, part2, part3]
+  ]
+}
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1.name} exercises1={part1.exercises1} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises}/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
